18798ad5e0cbbc058bb6869f35d01af49bfb5dda
Yassine El Khattabi <yassine@maroline.dev>
2025-04-09 15:04:38 +0100

    Add markdown parsing and run buttons to normal testdriver chatbot

--- src/chat.ts
+++ src/chat.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 import { TDInstance } from './cli';
-import { getActiveWorkspaceFolder } from './utils';
+import { getActiveWorkspaceFolder, MarkdownStreamParser } from './utils';
 import spec from './spec';
 
 export const PARTICIPANT_ID = 'testdriver.driver';
@@ -10,10 +10,10 @@ export function registerChatParticipant(_context: vscode.ExtensionContext) {
 }
 
 const handler: vscode.ChatRequestHandler = async (
-  request: vscode.ChatRequest,
-  _context: vscode.ChatContext,
-  stream: vscode.ChatResponseStream,
-  token: vscode.CancellationToken,
+  request,
+  context,
+  stream,
+  token,
 ): Promise<void> => {
   if (request.command) {
     const commands = ['dry', 'try'];
@@ -56,42 +56,56 @@ const handler: vscode.ChatRequestHandler = async (
     }
     return;
   } else {
-
     stream.progress('Staring my engine...');
-
-    console.log(_context.history)
-
-
+    console.log(context.history);
 
     try {
-        const messages = [
-            vscode.LanguageModelChatMessage.User(spec),            ];
+      const messages = [vscode.LanguageModelChatMessage.User(spec)];
 
-        // get all the previous participant messages
-        const previousMessages = _context.history.filter(
-          h => h instanceof vscode.ChatResponseTurn
-        );
+      // get all the previous participant messages
+      const previousMessages = context.history.filter(
+        (h) => h instanceof vscode.ChatResponseTurn,
+      );
 
-        // add the previous messages to the messages array
-        previousMessages.forEach(m => {
-          let fullMessage = '';
-          m.response.forEach(r => {
-            const mdPart = r as vscode.ChatResponseMarkdownPart;
-            fullMessage += mdPart.value.value;
-          });
-          messages.push(vscode.LanguageModelChatMessage.Assistant(fullMessage));
+      // add the previous messages to the messages array
+      previousMessages.forEach((m) => {
+        let fullMessage = '';
+        m.response.forEach((r) => {
+          const mdPart = r as vscode.ChatResponseMarkdownPart;
+          fullMessage += mdPart.value.value;
         });
+        messages.push(vscode.LanguageModelChatMessage.Assistant(fullMessage));
+      });
 
-        // add in the user's message
-        messages.push(vscode.LanguageModelChatMessage.User(request.prompt));
+      // add in the user's message
+      messages.push(vscode.LanguageModelChatMessage.User(request.prompt));
 
-        // send the request
-        const chatResponse = await request.model.sendRequest(messages, {}, token);
+      // send the request
+      const chatResponse = await request.model.sendRequest(messages, {}, token);
 
-        for await (const fragment of chatResponse.text) {
-            stream.markdown(fragment);
+      const parser = new MarkdownStreamParser();
+      parser
+        .on('markdown', (event) => {
+          stream.markdown(event);
+        })
+        .on('codeblock', (event) => {
+          stream.markdown(
+            `\n\n\`\`\`${event.type ?? ''}\n${event.content}\n\`\`\`\n\n`,
+          );
+          if (['yaml', 'yml'].includes(event.type?.toLowerCase() ?? '')) {
+            stream.button({
+              command: 'testdriver.codeblock.run',
+              title: vscode.l10n.t('Run Steps'),
+              arguments: [event.content], // Send the YML code as an argument
+            });
+          }
+        });
+      for await (const fragment of chatResponse.text) {
+        for (const char of fragment) {
+          parser.processChar(char);
         }
-
+      }
+      parser.end();
     } catch (err) {
       console.log('err', err);
     }
