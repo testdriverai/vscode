20228704b02df936e8d84befb871e300d4bf8ef8
Ian Jennings <ian@meetjennings.com>
2025-04-13 17:41:21 -0500

    revert partial running

--- src/cli.ts
+++ src/cli.ts
@@ -321,7 +321,9 @@ export class TDInstance extends EventEmitter<EventsMap> {
 }
 
 let chatInstance: TDInstance | null = null;
+
 export const getChatInstance = async () => {
+
   console.log('getChatInstance', chatInstance);
 
   if (!chatInstance || chatInstance.state === 'exit') {
@@ -379,27 +381,4 @@ export const getChatInstance = async () => {
 
   }
   return chatInstance;
-};
-
-let testInstance: TDInstance | null = null;
-export const getTestInstance = async () => {
-  console.log('getTestInstance', testInstance);
-
-  if (!testInstance || testInstance.state === 'exit') {
-    console.log('Creating new test instance');
-    const workingDir = getActiveWorkspaceFolder()?.uri.fsPath;
-    let env: Record<string, string> = {};
-    if (workingDir) {
-      const envPath = path.join(workingDir, '.env');
-      if (fs.existsSync(envPath)) {
-        const file = await vscode.workspace.fs.readFile(
-          vscode.Uri.file(envPath),
-        );
-        env = dotenv.parse(file.toString());
-        console.log('env', env);
-      }
-    }
-    testInstance = new TDInstance(workingDir ?? os.tmpdir(), { env });
-  }
-  return testInstance;
 };
--- src/utils.ts
+++ src/utils.ts
@@ -41,7 +41,7 @@ export async function isFileType(
   );
 }
 
-export function unslugify(name: string) {
+export function beautifyFilename(name: string) {
   return name
     .split('/')
     .pop()!
@@ -141,18 +141,3 @@ export class MarkdownStreamParser extends EventEmitter<{
     this.codeBlockContent = '';
   }
 }
-
-export const asyncFilter = async <T>(
-  array: T[],
-  predicate: (item: T) => Promise<boolean>,
-): Promise<T[]> => {
-  const results: T[] = [];
-
-  for (const item of array) {
-    if (await predicate(item)) {
-      results.push(item);
-    }
-  }
-
-  return results;
-};
